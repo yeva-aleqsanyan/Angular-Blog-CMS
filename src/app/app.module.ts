d645d54311885f78b5710134b44dc69222a30dff
yeva-aleqsanyan <yeva.aleqsanyan.fd@gmail.com>
2024-02-22 16:27:25 +0400

    Added structure with material design to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { RouterModule } from '@angular/router';
@@ -14,7 +16,10 @@ import { AfService } from './providers/af.service';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AdminPageComponent } from './admin-page/admin-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component'; 
+
+import { MaterialModule } from './material.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
 
 @NgModule({
   declarations: [
@@ -27,13 +32,15 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    MaterialModule,
   ],
-  providers: [AfService],
+  providers: [AfService, provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+    declarations: [
+        
+    ],
+    imports: [
+        MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule
+    ],
+    exports: [
+        MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule
+        
+    ],
+    providers: [],
+    bootstrap: []
+  })
+  export class MaterialModule { }
+  
