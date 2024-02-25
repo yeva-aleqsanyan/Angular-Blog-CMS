41e498500b8653001b87a2ad14172ac42c529f00
yeva-aleqsanyan <yeva.aleqsanyan.fd@gmail.com>
2024-02-26 00:42:25 +0400

    Added posts service

--- src/app/service/posts/posts.service.ts
+++ src/app/service/posts/posts.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+export interface Post {
+  title: ""; 
+  url: ""; 
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getPosts(): Observable<Post[]> { 
+    return this.afs.collection("posts").snapshotChanges()
+      .pipe(
+        map(postChanges => {
+          return postChanges.map(post => {
+            const data = post.payload.doc.data() as Post;
+            const id = post.payload.doc.id;
+            return { id, ...data };
+          });
+        })
+      );
+  }
+
+  addPost(post: Post): Promise<any> { 
+    return this.afs.collection("posts").add(post);
+  }
+
+  deletePost(postId: string): Promise<void> { 
+    return this.afs.doc('posts/' + postId).delete();
+  }
+
+  updatePost(postId: string, post: Post): Promise<void> { 
+    return this.afs.doc('posts/' + postId).update(post);
+  }
 }
